b2328f4d01ec1057b7f1d6fb25dfd1f0a65b7c69
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2018-10-14 11:48:53 +0200

    Added Logging

--- src/main/java/org/mybatis/jpetstore/service/AccountService.java
+++ src/main/java/org/mybatis/jpetstore/service/AccountService.java
@@ -47,6 +47,7 @@ public class AccountService {
    */
   @Transactional
   public void insertAccount(Account account) {
+    System.out.println("Account : " + account + " " + account.getCity());
     accountMapper.insertAccount(account);
     accountMapper.insertProfile(account);
     accountMapper.insertSignon(account);
